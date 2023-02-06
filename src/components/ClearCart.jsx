f9a38e864d43feebdc739401b2f448ac3c7a13fd
Erickc1602 <ecarv97@gmail.com>
2023-02-05 20:45:28 -0500

    logoutBtn update

--- src/components/ClearCart.jsx
+++ src/components/ClearCart.jsx
@@ -1,11 +1,14 @@
 import React, { useDispatch } from "react-redux";
 import { clearCart } from "../redux/cartRedux";
+import { useNavigate } from "react-router-dom";
 
 function ClearCartButton() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     dispatch(clearCart());
+    navigate("/");
   };
 
   return <button onClick={handleClick}>Clear Cart</button>;
