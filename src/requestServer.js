1fbb2cccb2057f0a2274509cf0a8afd27b2d130c
Erickc1602 <ecarv97@gmail.com>
2023-02-11 19:43:18 -0500

    new Backend

--- src/requestServer.js
+++ src/requestServer.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "https://goated.herokuapp.com/api";
+const BASE_URL = "https://goated.onrender.com/api";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDU4NDdjNWEwNzk4OTM0Y2M5M2ZjYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NTIzNzU4OSwiZXhwIjoxNjc1NDk2Nzg5fQ.AU8F_cA81C8MbRLsDaIvn_JSdcc7rMujH3Ax-l-d4IY"
 
 export const publicRequest = axios.create({
